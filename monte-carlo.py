40ad7baca215faa8a27d746edd60405b31b8c323
TurboWafflz <turbowafflz@gmail.com>
2025-03-20 17:46:53 -0400

    Tidy up whitespace

--- monte-carlo.py
+++ monte-carlo.py
@@ -12,26 +12,25 @@ def contact_customer():
 
     return (False, time)
 
+
 # Find the time a call attempt took
 # Returns (successful call, time taken)
 def call():
     time = 0
-    
+
     # Dial call
     time += 6
-    
+
     # Give up after 3 seconds if busy
     if busy:
         time += 3
         return (False, time)
-    
+
     # Give up if not answered after 25 seconds
     if away_from_phone or time_to_answer >= 25:
         time += 25
         return (False, time)
-   
+
     # Otherwise the call was successful
     time += time_to_answer
     return (True, time)
-
-
