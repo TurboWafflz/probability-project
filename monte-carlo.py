be171ffc322e4ac76b3532178d3bb9f90bd829c1
TurboWafflz <turbowafflz@gmail.com>
2025-03-20 17:42:22 -0400

    general flow of the thing

---
+++ monte-carlo.py
@@ -0,0 +1,37 @@
+# Find the time to contact a customer
+# Returns (successfully contacted, time to contact)
+def contact_customer():
+    time = 0
+
+    for i in range(4):
+        result = call()
+        time += result[1]
+
+        if result[0]:
+            return (True, time)
+
+    return (False, time)
+
+# Find the time a call attempt took
+# Returns (successful call, time taken)
+def call():
+    time = 0
+    
+    # Dial call
+    time += 6
+    
+    # Give up after 3 seconds if busy
+    if busy:
+        time += 3
+        return (False, time)
+    
+    # Give up if not answered after 25 seconds
+    if time_to_answer >= 25:
+        time += 25
+        return (False, time)
+   
+    # Otherwise the call was successful
+    time += time_to_answer
+    return (True, time)
+
+
